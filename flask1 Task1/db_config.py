49d70b371c44f0596847603336e3e510034f0efe
bhavithareddy815-max <bhavithareddy815@gmail.com>
2026-04-14 15:46:41 +0530

    Add files via upload

---
+++ flask1 Task1/db_config.py
@@ -0,0 +1,8 @@
+import mysql.connector
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Bhavi__15",
+        database="student_db1"
+    )
